0a50c06a1947d9ec4148a7fab2ef5a57665ee4bd
Risto Toivonen <risto@ergonomiapalvelu.fi>
2025-05-14 15:47:52 +0300

    Implode: PHP 8.0: Passing the separator after the array is no longer supported.

--- behaviors/ControllerBehavior.php
+++ behaviors/ControllerBehavior.php
@@ -17,7 +17,6 @@ use yii\helpers\Json;
 
 class ControllerBehavior extends Behavior
 {
-
     public function events()
     {
         return [
@@ -33,71 +32,82 @@ class ControllerBehavior extends Behavior
     {
 
         // Get stats module
-        if (Yii::$app->hasModule('admin/stats'))
+        if (Yii::$app->hasModule('admin/stats')) {
             $module = Yii::$app->getModule('admin/stats');
-        else
+        } else {
             $module = Yii::$app->getModule('stats');
+        }
 
         // Get stats options
-        if (isset(Yii::$app->params['stats.ignoreDev']))
+        if (isset(Yii::$app->params['stats.ignoreDev'])) {
             $ignoreDev = Yii::$app->params['stats.ignoreDev'];
-        else
+        } else {
             $ignoreDev = $module->ignoreDev;
+        }
 
-        if (isset(Yii::$app->params['stats.ignoreAjax']))
+        if (isset(Yii::$app->params['stats.ignoreAjax'])) {
             $ignoreAjax = Yii::$app->params['stats.ignoreAjax'];
-        else
+        } else {
             $ignoreAjax = $module->ignoreAjax;
+        }
 
-        if (isset(Yii::$app->params['stats.ignoreRoute']))
+        if (isset(Yii::$app->params['stats.ignoreRoute'])) {
             $ignoreRoute = Yii::$app->params['stats.ignoreRoute'];
-        else
+        } else {
             $ignoreRoute = $module->ignoreRoute;
+        }
 
-        if (isset(Yii::$app->params['stats.ignoreListIp']))
+        if (isset(Yii::$app->params['stats.ignoreListIp'])) {
             $ignoreListIp = Yii::$app->params['stats.ignoreListIp'];
-        else
+        } else {
             $ignoreListIp = $module->ignoreListIp;
+        }
 
-        if (isset(Yii::$app->params['stats.ignoreListUA']))
+        if (isset(Yii::$app->params['stats.ignoreListUA'])) {
             $ignoreListUA = Yii::$app->params['stats.ignoreListUA'];
-        else
+        } else {
             $ignoreListUA = $module->ignoreListUA;
+        }
 
-        if (isset(Yii::$app->params['stats.cookieName']))
+        if (isset(Yii::$app->params['stats.cookieName'])) {
             $cookieName = Yii::$app->params['stats.cookieName'];
-        else
+        } else {
             $cookieName = $module->cookieName;
+        }
 
-        if (isset(Yii::$app->params['stats.cookieExpire']))
+        if (isset(Yii::$app->params['stats.cookieExpire'])) {
             $cookieExpire = Yii::$app->params['stats.cookieExpire'];
-        else
+        } else {
             $cookieExpire = $module->cookieExpire;
+        }
 
-        if (isset(Yii::$app->params['stats.storagePeriod']))
+        if (isset(Yii::$app->params['stats.storagePeriod'])) {
             $storagePeriod = Yii::$app->params['stats.storagePeriod'];
-        else
+        } else {
             $storagePeriod = $module->storagePeriod;
+        }
 
-        if (isset(Yii::$app->params['stats.detectLocation']))
+        if (isset(Yii::$app->params['stats.detectLocation'])) {
             $detectLocation = Yii::$app->params['stats.detectLocation'];
-        else
+        } else {
             $detectLocation = $module->detectLocation;
+        }
 
 
-        if (($ignoreDev && (YII_DEBUG || YII_ENV == 'dev')) || ($ignoreAjax && Yii::$app->request->isAjax))
+        if (($ignoreDev && (YII_DEBUG || YII_ENV == 'dev')) || ($ignoreAjax && Yii::$app->request->isAjax)) {
             return;
+        }
 
         // Setup GeoIp2 Database reader
         $reader = null;
         if ($detectLocation) {
-
             $locale = \Locale::getPrimaryLanguage(Yii::$app->language); // Get short locale string
-            if (!$locale)
+            if (!$locale) {
                 $locale = 'en';
+            }
 
             try {
-                $reader = new \GeoIp2\Database\Reader(__DIR__ .'/../database/GeoLite2-Country.mmdb', [$locale]);
+                $reader = new \GeoIp2\Database\Reader(__DIR__ . '/../database/GeoLite2-Country.mmdb', [$locale]);
             } catch (Exception $e) {
                 Yii::debug($e->getMessage(), __METHOD__);
             }
@@ -109,8 +119,9 @@ class ControllerBehavior extends Behavior
         // Ignoring by route
         if (count($ignoreRoute) > 0) {
             foreach ($ignoreRoute as $route) {
-                if(preg_match('/('.preg_quote($route,'/').')/i', $request->url) || preg_match('/('.preg_quote($route,'/').')/i', $request->url))
+                if (preg_match('/(' . preg_quote($route, '/') . ')/i', $request->url) || preg_match('/(' . preg_quote($route, '/') . ')/i', $request->url)) {
                     return;
+                }
             }
         }
 
@@ -123,11 +134,10 @@ class ControllerBehavior extends Behavior
 
         // Ignoring by User Agent
         if (count($ignoreListUA) > 0) {
-            foreach($ignoreListUA as $user_agent) {
-
-                if(stripos($request->userAgent, $user_agent) !== false)
+            foreach ($ignoreListUA as $user_agent) {
+                if (stripos($request->userAgent, $user_agent) !== false) {
                     return;
-
+                }
             }
         }
 
@@ -166,22 +176,24 @@ class ControllerBehavior extends Behavior
                 $visitor->iso_code = strtolower($record->country->isoCode);
 
                 $params = [];
-                if (!is_array($visitor->params))
+                if (!is_array($visitor->params)) {
                     $params = $visitor->params;
+                }
 
                 $params['iso_code'] = strtolower($record->country->isoCode);
 
-                if (isset($record->country->name))
+                if (isset($record->country->name)) {
                     $params['country'] = $record->country->name;
-
+                }
             }
             $visitor->params = serialize($params);
         } else {
             $visitor->params = serialize($visitor->params);
         }
 
-        if ($visitor->save())
+        if ($visitor->save()) {
             $module->setVisitor($visitor);
+        }
 
         if ($storagePeriod !== 0 && rand(1, 10) == 1) {
             $period = (time() - (intval($storagePeriod) * 86400));
@@ -207,8 +219,9 @@ class ControllerBehavior extends Behavior
     public static function getRemoteIp($request)
     {
         $client_ip = $request->userIP;
-        if (!$client_ip)
+        if (!$client_ip) {
             $client_ip = $request->remoteIP;
+        }
 
         //return rand(195, 200).'.'.rand(120, 195).'.'.rand(150, 250).'.'.rand(1, 250); // For testing only
         return $client_ip;
@@ -224,11 +237,13 @@ class ControllerBehavior extends Behavior
         $client_ip = self::getRemoteIp($request);
 
         $host_name = $request->userHost;
-        if(!$host_name)
+        if (!$host_name) {
             $host_name = $request->remoteHost;
+        }
 
-        if(!$host_name)
+        if (!$host_name) {
             $host_name = gethostbyaddr($client_ip);
+        }
 
         return $host_name;
     }
@@ -241,13 +256,14 @@ class ControllerBehavior extends Behavior
     public static function checkUnique($session)
     {
         $count = Visitors::find()->where([
-            'session'=> $session
+            'session' => $session
         ])->count();
 
-        if($count > 0)
+        if ($count > 0) {
             return 0;
-        else
+        } else {
             return 1;
+        }
     }
 
     /**
@@ -264,7 +280,7 @@ class ControllerBehavior extends Behavior
 
         if (count($robots) > 0) {
             foreach ($robots as $robot) {
-                if (!empty($robot["regexp"]) && preg_match("/".preg_quote($robot["regexp"], "/")."/i", $user_agent)) {
+                if (!empty($robot["regexp"]) && preg_match("/" . preg_quote($robot["regexp"], "/") . "/i", $user_agent)) {
                     return $robot["id"];
                 }
             }
@@ -282,38 +298,43 @@ class ControllerBehavior extends Behavior
     {
 
         // Get stats module
-        if (Yii::$app->hasModule('admin/stats'))
+        if (Yii::$app->hasModule('admin/stats')) {
             $module = Yii::$app->getModule('admin/stats');
-        else
+        } else {
             $module = Yii::$app->getModule('stats');
-
-        if(preg_match('/(?!&)utm_([a-z0-9=%]+)/i', $request->getReferrer()) || preg_match('/(?!&)utm_([a-z0-9=%]+)/i', $request->getUrl()))
-            return Visitors::TYPE_FROM_ADVERTS;
-
-        if (count($module->advertisingSystems) > 0) {
-            $patterns = implode($module->advertisingSystems, "|");
-            if(preg_match('/('.$patterns.')/i', $request->getReferrer()) || preg_match('/('.$patterns.')/i', $request->getUrl()))
-                return Visitors::TYPE_FROM_ADVERTS;
         }
 
-        if ($request->getReferrer() === null)
+        if (preg_match('/(?!&)utm_([a-z0-9=%]+)/i', $request->getReferrer()) || preg_match('/(?!&)utm_([a-z0-9=%]+)/i', $request->getUrl())) {
+            return Visitors::TYPE_FROM_ADVERTS;
+        }
+
+        if (count($module->advertisingSystems) > 0) {
+            $patterns = implode("|", $module->advertisingSystems);
+            if (preg_match('/(' . $patterns . ')/i', $request->getReferrer()) || preg_match('/(' . $patterns . ')/i', $request->getUrl())) {
+                return Visitors::TYPE_FROM_ADVERTS;
+            }
+        }
+
+        if ($request->getReferrer() === null) {
             return Visitors::TYPE_DERECT_ENTRY;
-        else if (preg_match("($request->hostName)", $request->getReferrer()))
+        } elseif (preg_match("($request->hostName)", $request->getReferrer())) {
             return Visitors::TYPE_INNER_VISIT;
+        }
 
         if (count($module->searchEngines) > 0) {
-            $patterns = implode($module->searchEngines, "|");
-            if(preg_match('/('.$patterns.')/i', $request->getReferrer()))
+            $patterns = implode("|", $module->searchEngines);
+            if (preg_match('/(' . $patterns . ')/i', $request->getReferrer())) {
                 return Visitors::TYPE_FROM_SEARCH;
+            }
         }
 
         if (count($module->socialNetworks) > 0) {
-            $patterns = implode($module->socialNetworks, "|");
-            if(preg_match('/('.$patterns.')/i', $request->getReferrer()))
+            $patterns = implode("|", $module->socialNetworks);
+            if (preg_match('/(' . $patterns . ')/i', $request->getReferrer())) {
                 return Visitors::TYPE_FROM_SOCIALS;
+            }
         }
 
         return Visitors::TYPE_UNDEFINED;
     }
-
 }
